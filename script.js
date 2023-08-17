cab9cbbbd9f17b4f4cfc8486b50cc599c0c26ce5
leo312002 <142529075+leo312002@users.noreply.github.com>
2023-08-17 19:13:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+/* COUNTER VISITE ALLA PAGINA */
+const count = document.getElementById("count");
+
+incrementVisitsCount();
+
+function incrementVisitsCount(){
+    let visits;
+
+    if(!localStorage.getItem("visits")){
+        localStorage.setItem("visits", 1);
+    }else{
+
+    visits =+localStorage.getItem("visits");
+    const incrementedCount = visits + 1;
+
+    localStorage.setItem("visits", incrementedCount);
+    }
+    
+    count.innerText = localStorage.getItem("visits");
+}
