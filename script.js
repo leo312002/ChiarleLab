b877f3e8de502a428848a7952444970ad672f72b
leo312002 <142529075+leo312002@users.noreply.github.com>
2023-09-09 23:36:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ function incrementVisitsCount() {
 }
 
 /* chiude il collapse quando viene schiacciato un item della navbar */
-
 $(".js-scroll-trigger").click(function () {
   $(".navbar-collapse").collapse("hide");
 });
@@ -37,9 +36,73 @@ btnFormerMembers.addEventListener("click", function handleClick() {
       .includes(initialText.toLowerCase())
   ) {
     btnFormerMembers.innerHTML = "SEE FORMER MEMBERS";
-    document.getElementById("a-former-members").href = "#former-hide";
+    document.getElementById("a-former-members").href="#former-hide";
   } else {
     btnFormerMembers.textContent = initialText;
-    document.getElementById("a-former-members").href = "#former-show";
+    document.getElementById("a-former-members").href="#former-show";
   }
 });
+
+/* Script to change opacity when you scroll the web page */
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(window.innerWidth < 768){
+      $('.scroll-op-title').css("opacity", 0.7 + $(window).scrollTop()/700)
+      $('.scroll-op-first').css("opacity", 0.2)
+      $('.scroll-op-second').css("opacity", 0.2)
+      $('.scroll-op-third').css("opacity", 0.2)
+      if($(window).scrollTop() > 200){
+        $('.scroll-op-first').css("opacity", 1)
+      }
+      if($(window).scrollTop() > 800){
+        $('.scroll-op-first').css("opacity", 0.2)
+        $('.scroll-op-second').css("opacity", 1)
+      }
+      if($(window).scrollTop() > 1300){
+        $('.scroll-op-second').css("opacity", 0.2)
+        $('.scroll-op-third').css("opacity", 1)
+      }
+      if($(window).scrollTop() > 1700){
+        $('.scroll-op-third').css("opacity", 0.2)
+      }
+    }else if(window.innerWidth < 992){
+      $('.scroll-op-title').css("opacity", 0.7 + $(window).scrollTop()/700)
+      $('.scroll-op-first').css("opacity", 0.2)
+      $('.scroll-op-second').css("opacity", 0.2)
+      $('.scroll-op-third').css("opacity", 0.2)
+      if($(window).scrollTop() > 200){
+        $('.scroll-op-first').css("opacity", 1)
+      }
+      if($(window).scrollTop() > 700){
+        $('.scroll-op-first').css("opacity", 0.2)
+        $('.scroll-op-second').css("opacity", 1)
+      }
+      if($(window).scrollTop() > 1200){
+        $('.scroll-op-second').css("opacity", 0.2)
+        $('.scroll-op-third').css("opacity", 1)
+      }
+      if($(window).scrollTop() > 1500){
+        $('.scroll-op-third').css("opacity", 0.2)
+      }
+    }else{
+      $('.scroll-op-title').css("opacity", 0.7 + $(window).scrollTop()/700)
+      $('.scroll-op-first').css("opacity", 0.2)
+      $('.scroll-op-second').css("opacity", 0.2)
+      $('.scroll-op-third').css("opacity", 0.2)
+      if($(window).scrollTop() > 300){
+        $('.scroll-op-first').css("opacity", 1)
+      }
+      if($(window).scrollTop() > 800){
+        $('.scroll-op-first').css("opacity", 0.2)
+        $('.scroll-op-second').css("opacity", 1)
+      }
+      if($(window).scrollTop() > 1200){
+        $('.scroll-op-second').css("opacity", 0.2)
+        $('.scroll-op-third').css("opacity", 1)
+      }
+      if($(window).scrollTop() > 1700){
+        $('.scroll-op-third').css("opacity", 0.2)
+      }
+    }
+  })
+})
